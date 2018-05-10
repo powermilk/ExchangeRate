434ca984ada17dada5e71f5f6640a3dd8ed9eb19
Rafał Kociniewski <rafal.kociniewski@gmail.com>
2018-05-10 12:24:04 +0200

    added equals(), hashcode() and toString() methods

--- src/main/java/com/powermilk/ExchangeDataPOJO.java
+++ src/main/java/com/powermilk/ExchangeDataPOJO.java
@@ -1,42 +1,80 @@
 package com.powermilk;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.Contract;
+
 import java.util.List;
 
 public class ExchangeDataPOJO {
-    private String table;
-    private String currency;
-    private String code;
-    private List<RatePOJO> rates;
+    private final String table;
+    private final String currency;
+    private final String code;
+    private final List<RatePOJO> rates;
+
+    public ExchangeDataPOJO(String table, String currency, String code, List<RatePOJO> rates) {
+        this.table = table;
+        this.currency = currency;
+        this.code = code;
+        this.rates = rates;
+    }
+
+    public ExchangeDataPOJO() {
+        this.table = null;
+        this.currency = null;
+        this.code = null;
+        this.rates = null;
+    }
 
     public String getTable() {
         return table;
-    }
-
-    public void setTable(String table) {
-        this.table = table;
     }
 
     public String getCurrency() {
         return currency;
     }
 
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
     public String getCode() {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public List<RatePOJO> getRates() {
+    List<RatePOJO> getRates() {
         return rates;
     }
 
-    public void setRates(List<RatePOJO> rates) {
-        this.rates = rates;
+    @Override
+    @Contract(value = "null -> false", pure = true)
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final ExchangeDataPOJO exchangeDataPOJO = (ExchangeDataPOJO) obj;
+
+        return new EqualsBuilder()
+                .append(this.table, exchangeDataPOJO.table)
+                .append(this.currency, exchangeDataPOJO.currency)
+                .append(this.code, exchangeDataPOJO.code)
+                .append(this.rates, exchangeDataPOJO.rates)
+                .build();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(table).append(currency).append(code).append(rates).build();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
--- src/main/java/com/powermilk/RatePOJO.java
+++ src/main/java/com/powermilk/RatePOJO.java
@@ -1,31 +1,70 @@
 package com.powermilk;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.Contract;
+
 public class RatePOJO {
-    private String no;
-    private String effectiveDate;
-    private double mid;
+    private final String no;
+    private final String effectiveDate;
+    private final double mid;
+
+    public RatePOJO() {
+        this.no = null;
+        this.effectiveDate = null;
+        mid = 0.0d;
+    }
+
+    public RatePOJO(String no, String effectiveDate, double mid) {
+        this.no = no;
+        this.effectiveDate = effectiveDate;
+        this.mid = mid;
+    }
 
     public String getNo() {
         return no;
-    }
-
-    public void setNo(String no) {
-        this.no = no;
     }
 
     public String getEffectiveDate() {
         return effectiveDate;
     }
 
-    public void setEffectiveDate(String effectiveDate) {
-        this.effectiveDate = effectiveDate;
-    }
-
-    public double getMid() {
+    double getMid() {
         return mid;
     }
 
-    public void setMid(double mid) {
-        this.mid = mid;
+    @Override
+    @Contract(value = "null -> false", pure = true)
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final RatePOJO ratePOJO = (RatePOJO) obj;
+
+        return new EqualsBuilder()
+                .append(this.no, ratePOJO.no)
+                .append(this.effectiveDate, ratePOJO.effectiveDate)
+                .append(this.mid, ratePOJO.mid)
+                .build();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(no).append(effectiveDate).append(mid).build();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
